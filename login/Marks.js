dfd21f3022ef764732a784410363dfba80b9e3aa
Piyush Motwani <piyushmotwani2@gmail.com>
2020-06-19 23:09:18 +0530

    Tabs added

--- login/Marks.js
+++ login/Marks.js
@@ -1,355 +1,53 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  Image
-} from 'react-native';
+import * as React from 'react';
+import { TouchableOpacity, StyleSheet, View, Text, SafeAreaView } from 'react-native';
 
-const Assignment = () =>  {
-  return(
-    <View style = {
-      {
-        "flexDirection": "row",
-        "alignItems": "flex-start",
-        "flex": 1
-      }
-    } >
-    {/*<View style = {
-      {
-        "alignItems": "flex-start",
-        "width": 720,
-        "height": 312,
-        "backgroundColor": "rgba(255, 255, 255, 255)"
-      }
-    }
-    />
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "paddingStart": 26,
-        "paddingTop": 32,
-        "marginStart": -680,
-        "marginTop": 72,
-        "width": 640,
-        "height": 1136,
-        "borderWidth": 2,
-        "borderColor": "rgba(64, 51, 238, 255)"
-      }
-    } >
-    <View style = {
-      {
-        flexDirection: "row",
-        alignItems: "flex-start"
-      }
-    } > <View style = {
-      {
-        "alignItems": "flex-start"
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "paddingStart": 21,
-        "paddingTop": 18,
-        "width": 75,
-        "height": 75,
-        "borderRadius": 37.5,
-        "backgroundColor": "rgba(255, 255, 255, 255)"
-      }
-    } >
-
-    {
-      /* <Path /> {Path is not supported. It can be exported as Svg}  }
-    <View style = {
-      {
-        "width": 33.85,
-        "height": 40,
-        "backgroundColor": "#000000"
-      }
-    }
-    />
-    </View>
-    </View>
-    <Image style = {
-      {
-        "marginStart": 439,
-        "width": 75,
-        "height": 75,
-        "borderRadius": 37.5
-      }
-    }
-    source = {
-      {
-        /* add your source here  }
-    }
-    />
-    </View>
-    <View style = {
+const Marks = ({ navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor:"#FFF", alignItems:"center"}}>
+      <View style = {
       {
         flexDirection: 'row',
-        alignItems: 'flex-start'
+        alignItems: 'center'
       }
-    } > <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 3,
-        "marginTop": 60
-      }
-    } > Schedule </Text>
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(108, 92, 189, 255)",
-        "marginStart": 313,
-        "marginTop": 60
-      }
-    } > Marks </Text>
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(161, 154, 154, 255)",
-        "marginStart": -349,
-        "marginTop": 60
-      }
-    } > Attendance </Text>
-    </View>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginStart": 425,
-        "marginTop": 33,
-        "width": 165,
-        "height": 5,
-        "borderRadius": 2.5,
-        "backgroundColor": "rgba(108, 92, 189, 255)"
-      }
-    }
-    />
-    <View style = {
+      } >
+        <Text style = {
+        {
+          "fontFamily": "SF UI Text",
+          "fontSize": 24,
+          "color": "rgba(18, 18, 18, 255)",
+          "marginTop": 47
+        }
+        } > Subject: </Text>
+        <View style = {
       {
         flexDirection: 'row',
-        alignItems: 'flex-start'
-      }
-    } > <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontSize": 32,
-        "color": "rgba(18, 18, 18, 255)",
-        "marginStart": 20,
-        "marginTop": 91
-      }
-    } > Subject: </Text>
-    <View style = {
-      {
         "alignItems": "flex-start",
         "paddingStart": 24,
-        "paddingTop": 21,
+        "paddingTop": 10,
         "marginStart": 34,
-        "marginTop": 75,
-        "width": 391,
-        "height": 70,
+        "marginTop": 47,
+        "width": 180,
+        "height": 40,
         "borderRadius": 35,
         "borderWidth": 1,
         "borderColor": "rgba(18, 18, 18, 255)"
       }
     } >
-    <Text style = {
+      <Text style = {
       {
         "fontFamily": "SF UI Text",
-        "fontSize": 24,
-        "color": "rgba(18, 18, 18, 255)"
-      }
-    } > CSE1001 - Introduction to Pro. </Text>
-    </View>
-    </View>
-    <View style = {
-      {
-        flexDirection: 'row',
-        alignItems: 'flex-start'
-      }
-    } > <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontSize": 32,
+        "fontSize": 14,
         "color": "rgba(18, 18, 18, 255)",
-        "marginStart": 11,
-        "marginTop": 90
+        "textAlign":"center"
       }
-    } > Assignment 1 </Text>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginStart": 34,
-        "marginTop": 89
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "width": 320,
-        "height": 40,
-        "borderRadius": 20,
-        "backgroundColor": "rgba(180, 175, 175, 255)"
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "paddingStart": 134.5,
-        "paddingTop": 5,
-        "width": 256,
-        "height": 40,
-        "borderTopStartRadius": 20,
-        "borderTopEndRadius": 0,
-        "borderBottomEndRadius": 0,
-        "borderBottomStartRadius": 20,
-        "backgroundColor": "rgba(108, 92, 189, 255)"
-      }
-    } >
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 24,
-        "color": "rgba(255, 255, 255, 255)"
-      }
-    } > 80 % </Text>
+    } > CSE1001 - Introdu. </Text>
     </View>
     </View>
     </View>
-    </View>
-    <View style = {
-      {
-        flexDirection: 'row',
-        alignItems: 'flex-start'
-      }
-    } > <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontSize": 32,
-        "color": "rgba(18, 18, 18, 255)",
-        "marginStart": 11,
-        "marginTop": 51
-      }
-    } > Assignment 2 </Text>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginStart": 30,
-        "marginTop": 50
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "width": 320,
-        "height": 40,
-        "borderRadius": 20,
-        "backgroundColor": "rgba(180, 175, 175, 255)"
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "paddingStart": 134.5,
-        "paddingTop": 5,
-        "width": 272,
-        "height": 40,
-        "borderTopStartRadius": 20,
-        "borderTopEndRadius": 0,
-        "borderBottomEndRadius": 0,
-        "borderBottomStartRadius": 20,
-        "backgroundColor": "rgba(108, 92, 189, 255)"
-      }
-    } >
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 24,
-        "color": "rgba(255, 255, 255, 255)"
-      }
-    } > 85 % </Text>
-    </View>
-    </View>
-    </View>
-    </View>
-    <View style = {
-      {
-        flexDirection: 'row',
-        alignItems: 'flex-start'
-      }
-    } > <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontSize": 32,
-        "color": "rgba(18, 18, 18, 255)",
-        "marginStart": 17,
-        "marginTop": 51
-      }
-    } > Assignment 3 </Text>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginStart": 29,
-        "marginTop": 50
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "width": 320,
-        "height": 40,
-        "borderRadius": 20,
-        "backgroundColor": "rgba(235, 137, 137, 255)"
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "paddingStart": 119.5,
-        "paddingTop": 6,
-        "width": 272,
-        "height": 40,
-        "borderTopStartRadius": 20,
-        "borderTopEndRadius": 0,
-        "borderBottomEndRadius": 0,
-        "borderBottomStartRadius": 20,
-        "backgroundColor": "rgba(235, 137, 137, 255)"
-      }
-    } >
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 24,
-        "color": "rgba(255, 255, 255, 255)"
-      }
-    } > Absent </Text>
-    </View>
-    </View>
-    </View>
-    </View>
-  </View>*/}
-  <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 55
-      }
-    } > Ethics and Values </Text>
-    </View>
-
-  )
+    </SafeAreaView>
+  );
 }
 
-export default Assignment
+
+export default Marks;
--- login/Schedule.js
+++ login/Schedule.js
@@ -1,150 +1,23 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  Image
-} from 'react-native';
+import * as React from 'react';
+import { TouchableOpacity, StyleSheet, View, Text, SafeAreaView } from 'react-native';
 
-const Schedule = () => {
-  return(
-    <View style = {
+const Schedule = ({ navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1,
+      backgroundColor:"#FFF"}}>
+      <View style = {
       {
         "flexDirection": "row",
-        "alignItems": "flex-start",
-        "flex": 1
-      }
-    } >
-    {/* <View style = {
-      {
-        "alignItems": "flex-start",
-        "width": 720,
-        "height": 312,
-        "backgroundColor": "rgba(255, 255, 255, 255)"
-      }
-    }
-    />
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "paddingStart": 20,
-        "paddingTop": 32,
-        "marginStart": -680,
-        "marginTop": 72,
-        "width": 640,
-        "height": 1136,
-        "borderWidth": 2,
-        "borderColor": "rgba(64, 51, 238, 255)"
-      }
-    } >
-    <View style = {
-      {
-        flexDirection: 'row',
-        alignItems: 'flex-start'
-      }
-    } > <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginStart": 6
-      }
-    } >
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "paddingStart": 21,
-        "paddingTop": 18,
-        "width": 75,
-        "height": 75,
-        "borderRadius": 37.5,
-        "backgroundColor": "rgba(255, 255, 255, 255)"
-      }
-    } >
-
-    {
-      /* <Path /> {Path is not supported. It can be exported as Svg}  }
-    <View style = {
-      {
-        "width": 33.85,
-        "height": 40,
-        "backgroundColor": "#000000"
-      }
-    }
-    />
-    </View>
-    </View>
-    <Image style = {
-      {
-        "marginStart": 439,
-        "width": 75,
-        "height": 75,
-        "borderRadius": 37.5
-      }
-    }
-    source = {
-      {
-        /* add your source here  }
-    }
-    />
-    </View>
-    <View style = {
-      {
-        flexDirection: 'row',
-        alignItems: 'flex-start'
-      }
-    } > <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(108, 92, 189, 255)",
-        "marginStart": 17,
-        "marginTop": 60
-      }
-    } > Schedule </Text>
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 305,
-        "marginTop": 60
-      }
-    } > Marks </Text>
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(161, 154, 154, 255)",
-        "marginStart": -341,
-        "marginTop": 60
-      }
-    } > Attendance </Text>
-    </View>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginStart": 6,
-        "marginTop": 30,
-        "width": 165,
-        "height": 5,
-        "borderRadius": 2.5,
-        "backgroundColor": "rgba(108, 92, 189, 255)"
-      }
-    }
-    />
-    <View style = {
-      {
-        "flexDirection": "row",
-        "alignItems": "flex-start",
-        "marginTop": 56
+        "alignItems": "center",
+        justifyContent: "center",
+        "padding":20
       }
     } >
     <Text style = {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
         "color": "rgba(161, 154, 154, 255)"
       }
     } > S </Text>
@@ -152,81 +25,87 @@ const Schedule = () => {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
         "color": "rgba(108, 92, 189, 255)",
-        "marginStart": 73
+        "marginStart": 36.5
       }
     } > M </Text>
     <Text style = {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
         "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 73
+        "marginStart": 36.5
       }
     } > T </Text>
     <Text style = {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
         "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 73
+        "marginStart": 36.5
       }
     } > W </Text>
     <Text style = {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
         "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 73
+        "marginStart": 36.5
       }
     } > T </Text>
     <Text style = {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
         "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 73
+        "marginStart": 36.5
       }
     } > F </Text>
     <Text style = {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
         "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 73
+        "marginStart": 36.5
       }
     } > S </Text>
     </View>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginTop": 56
-      }
-    } >
-    <Text style = {
+      <View style={
+        {
+          alignItems: "flex-start"
+        }
+      }>
+      <Text style = {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
+        "paddingTop":20,
+        "paddingLeft":30,
         "color": "rgba(161, 154, 154, 255)"
       }
     } > 8 AM </Text>
-    <View style = {
+      </View>
+      <View style={{
+        "alignItems": "center",
+        "paddingTop": 10
+      }}>
+        <View style = {
       {
         "flexDirection": "row",
-        "alignItems": "flex-start",
-        "paddingStart": 38,
-        "paddingTop": 21,
-        "marginTop": 24,
-        "width": 600,
-        "height": 80,
-        "borderRadius": 20,
+        "alignItems": "center",
+        "paddingStart": 5,
+        "paddingTop": 0,
+        "marginTop": 5,
+        "width": 375,
+        "height": 50,
+        "borderRadius": 10,
         "backgroundColor": "rgba(244, 244, 254, 255)"
       }
     } >
@@ -234,7 +113,10 @@ const Schedule = () => {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
+        "paddingLeft":15,
+        "marginLeft":"auto",
+        "marginRight":"auto",
         "color": "rgba(161, 154, 154, 255)"
       }
     } > MAT1001 </Text>
@@ -242,37 +124,46 @@ const Schedule = () => {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
+        "textAlign":"center",
+        "marginLeft":"auto",
+        "marginRight":"auto",
         "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 59
+        "marginStart": 29
       }
     } > Engineering Calculus </Text>
-    </View>
-    </View>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginTop": 30
-      }
-    } >
-    <Text style = {
+        </View>
+      </View>
+      <View style={
+        {
+          alignItems: "flex-start"
+        }
+      }>
+      <Text style = {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
+        "paddingTop":20,
+        "paddingLeft":30,
         "color": "rgba(108, 92, 189, 255)"
       }
     } > 9 AM </Text>
-    <View style = {
+      </View>
+      <View style={{
+        "alignItems": "center",
+        "paddingTop": 10
+      }}>
+        <View style = {
       {
         "flexDirection": "row",
-        "alignItems": "flex-start",
-        "paddingStart": 38,
-        "paddingTop": 56,
-        "marginTop": 26,
-        "width": 600,
-        "height": 150,
-        "borderRadius": 20,
+        "alignItems": "center",
+        "paddingStart": 5,
+        "paddingTop": 0,
+        "marginTop": 5,
+        "width": 375,
+        "height": 70,
+        "borderRadius": 10,
         "backgroundColor": "rgba(108, 92, 189, 255)"
       }
     } >
@@ -280,7 +171,10 @@ const Schedule = () => {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
+        "paddingLeft":15,
+        "marginLeft":"auto",
+        "marginRight":"auto",
         "color": "rgba(255, 255, 255, 255)"
       }
     } > CSE1001 </Text>
@@ -288,37 +182,46 @@ const Schedule = () => {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
+        "textAlign":"center",
+        "marginLeft":"auto",
+        "marginRight":"auto",
         "color": "rgba(255, 255, 255, 255)",
-        "marginStart": 64
+        "marginStart": 29
       }
     } > Introduction to Prog.. </Text>
-    </View>
-    </View>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginTop": 30
-      }
-    } >
-    <Text style = {
+        </View>
+      </View>
+      <View style={
+        {
+          alignItems: "flex-start"
+        }
+      }>
+      <Text style = {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
+        "paddingTop":20,
+        "paddingLeft":30,
         "color": "rgba(161, 154, 154, 255)"
       }
     } > 10 AM </Text>
-    <View style = {
+      </View>
+      <View style={{
+        "alignItems": "center",
+        "paddingTop": 10
+      }}>
+        <View style = {
       {
         "flexDirection": "row",
-        "alignItems": "flex-start",
-        "paddingStart": 144,
-        "paddingTop": 21,
-        "marginTop": 26,
-        "width": 600,
-        "height": 80,
-        "borderRadius": 20,
+        "alignItems": "center",
+        "paddingStart": 5,
+        "paddingTop": 0,
+        "marginTop": 5,
+        "width": 375,
+        "height": 50,
+        "borderRadius": 10,
         "backgroundColor": "rgba(95, 238, 180, 255)"
       }
     } >
@@ -326,7 +229,10 @@ const Schedule = () => {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
+        "paddingLeft":15,
+        "marginLeft":"auto",
+        "marginRight":"auto",
         "color": "rgba(255, 255, 255, 255)"
       }
     } > - </Text>
@@ -334,73 +240,18 @@ const Schedule = () => {
       {
         "fontFamily": "SF UI Text",
         "fontWeight": "400",
-        "fontSize": 32,
+        "fontSize": 18,
+        "textAlign":"center",
+        "marginLeft":"auto",
+        "marginRight":"auto",
         "color": "rgba(255, 255, 255, 255)",
-        "marginStart": 185
+        "marginStart": 29
       }
     } > Leisure </Text>
-    </View>
-    </View>
-    <View style = {
-      {
-        "alignItems": "flex-start",
-        "marginTop": 30
-      }
-    } >
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(161, 154, 154, 255)"
-      }
-    } > 11 AM </Text>
-    <View style = {
-      {
-        "flexDirection": "row",
-        "alignItems": "flex-start",
-        "paddingStart": 64,
-        "paddingTop": 21,
-        "marginTop": 24,
-        "width": 600,
-        "height": 80,
-        "borderRadius": 20,
-        "backgroundColor": "rgba(244, 244, 254, 255)"
-      }
-    } >
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(161, 154, 154, 255)"
-      }
-    } > MGT1001 </Text>
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 55
-      }
-    } > Ethics and Values </Text>
-    </View>
-    </View>
-    </View> */}
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 32,
-        "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 55
-      }
-    } > Ethics and Values </Text>
-    </View>
-
-  )
-
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
-export default Schedule
+export default Schedule;
