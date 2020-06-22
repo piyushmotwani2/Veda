bc7667a2a3a3239c7aec05d8e93e56c2ce1773f8
Piyush Motwani <piyushmotwani2@gmail.com>
2020-06-22 23:37:54 +0530

    CardComponent Added

--- login/Schedule.js
+++ login/Schedule.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { TouchableOpacity, StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import { TouchableOpacity, StyleSheet, View, Text, SafeAreaView, ScrollView } from 'react-native';
+import CardComponent from '../components/CardComponent';
 
 const Schedule = ({ navigation }) => {
   return (
-    <SafeAreaView style={{ flex: 1,
+    <ScrollView style={{ flex: 1,
       backgroundColor:"#FFF"}}>
       <View style = {
       {
@@ -76,6 +77,9 @@ const Schedule = ({ navigation }) => {
       }
     } > S </Text>
     </View>
+      <CardComponent time ="8 AM" subID = "MAT1001" subName = "Engineering Calculus" status = "inactive"/>
+      <CardComponent time ="9 AM" subID = "MAT1001" subName = "Engineering Calculus" status = "inactive"/>
+      <CardComponent time ="10 AM" subID = "MAT1001" subName = "Engineering Calculus" status = "active"/>
       <View style={
         {
           alignItems: "flex-start"
@@ -90,123 +94,7 @@ const Schedule = ({ navigation }) => {
         "paddingLeft":30,
         "color": "rgba(161, 154, 154, 255)"
       }
-    } > 8 AM </Text>
-      </View>
-      <View style={{
-        "alignItems": "center",
-        "paddingTop": 10
-      }}>
-        <View style = {
-      {
-        "flexDirection": "row",
-        "alignItems": "center",
-        "paddingStart": 5,
-        "paddingTop": 0,
-        "marginTop": 5,
-        "width": 375,
-        "height": 50,
-        "borderRadius": 10,
-        "backgroundColor": "rgba(244, 244, 254, 255)"
-      }
-    } >
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 18,
-        "paddingLeft":15,
-        "marginLeft":"auto",
-        "marginRight":"auto",
-        "color": "rgba(161, 154, 154, 255)"
-      }
-    } > MAT1001 </Text>
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 18,
-        "textAlign":"center",
-        "marginLeft":"auto",
-        "marginRight":"auto",
-        "color": "rgba(161, 154, 154, 255)",
-        "marginStart": 29
-      }
-    } > Engineering Calculus </Text>
-        </View>
-      </View>
-      <View style={
-        {
-          alignItems: "flex-start"
-        }
-      }>
-      <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 18,
-        "paddingTop":20,
-        "paddingLeft":30,
-        "color": "rgba(108, 92, 189, 255)"
-      }
-    } > 9 AM </Text>
-      </View>
-      <View style={{
-        "alignItems": "center",
-        "paddingTop": 10
-      }}>
-        <View style = {
-      {
-        "flexDirection": "row",
-        "alignItems": "center",
-        "paddingStart": 5,
-        "paddingTop": 0,
-        "marginTop": 5,
-        "width": 375,
-        "height": 70,
-        "borderRadius": 10,
-        "backgroundColor": "rgba(108, 92, 189, 255)"
-      }
-    } >
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 18,
-        "paddingLeft":15,
-        "marginLeft":"auto",
-        "marginRight":"auto",
-        "color": "rgba(255, 255, 255, 255)"
-      }
-    } > CSE1001 </Text>
-    <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 18,
-        "textAlign":"center",
-        "marginLeft":"auto",
-        "marginRight":"auto",
-        "color": "rgba(255, 255, 255, 255)",
-        "marginStart": 29
-      }
-    } > Introduction to Prog.. </Text>
-        </View>
-      </View>
-      <View style={
-        {
-          alignItems: "flex-start"
-        }
-      }>
-      <Text style = {
-      {
-        "fontFamily": "SF UI Text",
-        "fontWeight": "400",
-        "fontSize": 18,
-        "paddingTop":20,
-        "paddingLeft":30,
-        "color": "rgba(161, 154, 154, 255)"
-      }
-    } > 10 AM </Text>
+    } > 1 PM </Text>
       </View>
       <View style={{
         "alignItems": "center",
@@ -250,7 +138,7 @@ const Schedule = ({ navigation }) => {
     } > Leisure </Text>
         </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 }
 
